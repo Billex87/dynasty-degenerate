2fe2f9c26df91f7bcc113370770ca000bd309534
Billy McKinnon <billy_mckinnon@hotmail.com>
2026-06-01 08:45:32 -0700

    Restore centered home layout

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,7 +6,6 @@ import {
   type CSSProperties,
   type ReactNode,
 } from "react";
-import "@/styles/home-backgrounds-v12.css";
 import { trpc } from "@/lib/trpc";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
